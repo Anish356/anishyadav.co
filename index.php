d7495c3047ba88f2ccdbeaf7a6e57dcbbf6167c1
Anish Yadav <anishyadav@gmail.com>
2022-07-17 20:17:39 +0530

    my resume html

---
+++ index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head><meta charset="gb18030">
+	<title>Anish Yadav</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	
+	
+	<!-- Font -->
+	
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700%7CAllura" rel="stylesheet">
+	
+	<!-- Stylesheets -->
+	
+	<link href="common-css/bootstrap.css" rel="stylesheet">
+	
+	<link href="common-css/ionicons.css" rel="stylesheet">
+	
+	<link href="common-css/fluidbox.min.css" rel="stylesheet">
+	
+	<link href="02-cv-boxed/css/styles.css" rel="stylesheet">
+	
+	<link href="02-cv-boxed/css/responsive.css" rel="stylesheet">
+	
+</head>
+<body>
+	
+	<section class="intro-section">
+		<div class="container">
+		
+			<div class="heading-wrapper">
+				<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-location-outline"></i>
+							<div class="right-area">
+								<h5>Pragati Nagar ,Dipka</h5>
+								<h5>Korba (Chhattisgarh)</h5>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+					
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-telephone-outline"></i>
+							<div class="right-area">
+								<h5>9340663930</h5>
+								<h6>MIN - FRI,8AM - 7PM</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+					
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-chatboxes-outline"></i>
+							<div class="right-area">
+								<h5>admin@sagargupta.co</h5>
+								<h6>REPLY IN 24 HOURS</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+				</div><!-- row -->
+			</div><!-- heading-wrapper -->
+			
+		
+		
+			<div class="intro">
+				<div class="row">
+				
+					<div class="col-sm-8 col-md-4 col-lg-3">
+						<div class="profile-img margin-b-30"><img src="images/sagar/anish.jpg" alt=""></div>
+					</div><!-- col-sm-8 -->
+					<div class="col-sm-10 col-md-5 col-lg-6">		
+							<h2><b>Anish Yadav</b></h2>
+							<h4 class="font-yellow">Software trainee</h4>
+							<ul class="information margin-tb-30">
+								<li><b class="font-yellow">BORN</b> : July 10, 1999</li>
+								<li><b class="font-yellow">EMAIL</b> : anishyadav231998@gmail.com</li>
+								<li><b class="font-yellow">MARITAL STATUS</b> : UnMarried</li>
+							</ul>
+							<ul class="social-icons">
+								<li><a href="#"><i class="ion-social-pinterest"></i></a></li>
+								<li><a href="https://www.linkedin.com/in/sagargupta31"><i class="ion-social-linkedin"></i></a></li>
+								<li><a href="#"><i class="ion-social-instagram"></i></a></li>
+								<li><a href="https://www.facebook.com/profile.php?id=100009145205984"><i class="ion-social-facebook"></i></a></li>
+								<li><a href="#"><i class="ion-social-twitter"></i></a></li>
+							</ul>
+					</div><!-- col-sm-8 -->
+					
+					<div class="col-sm-10 col-md-3 col-lg-3">
+						<a class="downlad-btn" href="#">Download CV</a>
+					</div><!-- col-lg-2 -->
+			        <div class="col-sm-10 col-md-3 col-lg-3">
+						<a class="downlad-btn" href="#">ENTER WEBSITE</a>
+					</div><!-- col-lg-2 -->
+				</div><!-- row -->
+			
+			</div><!-- intro -->
+		</div><!-- container -->
+	</section><!-- intro-section -->
+	
+	
+	<section class="about-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-3">
+					<div class="heading">
+						<h3><b>About me</b></h3>
+						<h6 class="font-lite-black"><b>PROFESSIONAL PATH</b></h6>
+					</div>
+				</div><!-- col-sm-3 -->
+				<div class="col-sm-12 col-md-9">
+					<p class="margin-b-50">Hi there! I'm Anish  and I am budding Software Engineer. 
+						                I have worked on various projects which includes
+                                        Robotics, Artificial Intelligence,I am innovative ,creative and willing to 
+                                        contribute ideas and learn new things.
+                                        </p>
+
+					<div class="row">
+						<div class="col-sm-6 col-md-6 col-lg-3">
+							<div class="radial-prog-area margin-b-30">
+								<div class="radial-progress" data-prog-percent=".82">
+									<div></div>
+									<h6 class="progress-title"> <br> C++</h6>
+								</div>
+							</div><!-- radial-prog-area-->
+						</div><!-- col-sm-6-->
+					
+						<div class="col-sm-6 col-md-6 col-lg-3">
+							<div class="radial-prog-area margin-b-30">
+								<div class="radial-progress" data-prog-percent=".78">
+									<div></div>
+									<h6 class="progress-title"> <br>Python</h6>
+								</div>
+							</div><!-- radial-prog-area-->
+						</div><!-- col-sm-6-->
+						
+						<div class="col-sm-6 col-md-6 col-lg-3">
+							<div class="radial-prog-area margin-b-30">
+								<div class="radial-progress" data-prog-percent=".67">
+									<div></div>
+									<h6 class="progress-title"> <br>Dart</h6>
+								</div>
+							</div><!-- radial-prog-area-->
+						</div><!-- col-sm-6-->
+						
+						<div class="col-sm-6 col-md-6 col-lg-3">
+							<div class="radial-prog-area margin-b-50">
+								<div class="radial-progress" data-prog-percent=".85">
+									<div></div>
+									<h6 class="progress-title"><br>IOT</h6>
+								</div>
+							</div><!-- radial-prog-area-->
+						</div><!-- col-sm-6-->
+					
+					</div><!-- row -->
+				</div><!-- col-sm-9 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- about-section -->
+	
+	<section class="experience-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-3">
+					<div class="heading">
+						<h3><b>EDUCATION</b></h3>
+						<h6 class="font-lite-black"><b>ACADEMIC CAREER</b></h6>
+					</div>
+				</div><!-- col-sm-3 -->
+				
+				<div class="col-sm-12 col-md-9">
+					<div class="experience margin-b-50">
+						<h4><b>B.TECH</b></h4>
+						<h5 class="font-yellow"><b>2018-2022</b></h5>
+						<!--<h6 class="font-lite-black margin-t-10">2018-2022</h6>-->
+						<p class="margin-tb-20">GOVT ENGINEERING COLLEGE JAGDALPUR , Jagdalpur Chhattisgarh
+                                                 
+                        </p>
+						<p class="margin-tb-20">CGPA-8.4 (7th Sem) 
+                        </p>
+						<!--<ul class="list margin-b-30">
+							<li>Profitability impact of the company through ensuring strategic and tactical decision, liaising
+                                                with the School Principals, Business heads and Stake holders for the smooth functioning of
+                                                the business.</li>
+							<li>Conduct weekly department coordinators meetings to reinforce standards and procedures,
+                                                conduct performance evaluation for all coordinators.</li>
+							<li>Handeled a team size of 40-50 .</li>
+						</ul>-->
+					</div><!-- experience -->
+					
+					<div class="experience margin-b-50">
+						<h4><b>CBSE-XII</b></h4>
+						<h5 class="font-yellow"><b>2017-2018</b></h5>
+						<!--<h6 class="font-lite-black margin-t-10">2018-2022</h6>-->
+						<p class="margin-tb-20">CAREER POINT  WORLD SCHOOL,  Bilaspur Chhattisgarh 
+                        </p>
+						<p class="margin-tb-20">Percentage-80% (7th Sem) 
+                        </p>
+						<!--<ul class="list margin-b-30">
+							<li>Profitability impact of the company through ensuring strategic and tactical decision, liaising
+                                                with the School Principals, Business heads and Stake holders for the smooth functioning of
+                                                the business.</li>
+							<li>Conduct weekly department coordinators meetings to reinforce standards and procedures,
+                                                conduct performance evaluation for all coordinators.</li>
+							<li>Handeled a team size of 40-50 .</li>
+						</ul>-->
+					</div><!-- experience -->
+					
+					
+					<div class="experience margin-b-50">
+						<h4><b>CBSE-X</b></h4>
+						<h5 class="font-yellow"><b>2015-2016</b></h5>
+						<!--<h6 class="font-lite-black margin-t-10">2018-2022</h6>-->
+						<p class="margin-tb-20">KENDRIYA VIDAYALAYA KUSMUNDA Korba Chhattisgarh
+                        </p>
+						<p class="margin-tb-20">CGPA-8.8
+                        </p>
+						<!--<ul class="list margin-b-30">
+							<li>Profitability impact of the company through ensuring strategic and tactical decision, liaising
+                                                with the School Principals, Business heads and Stake holders for the smooth functioning of
+                                                the business.</li>
+							<li>Conduct weekly department coordinators meetings to reinforce standards and procedures,
+                                                conduct performance evaluation for all coordinators.</li>
+							<li>Handeled a team size of 40-50 .</li>
+						</ul>-->
+					</div><!-- experience -->					
+					
+			<!--		<div class="experience margin-b-50">
+						<h4><b>MechaToy Creator LLP</b></h4>
+						<h5 class="font-yellow"><b>Founder</b></h5>
+						<h6 class="font-lite-black margin-t-10">AUG 2016 - MAY 2018</h6>
+						<p class="margin-tb-30">
+                                    Designed different K-10 teaching tools aligned to various concepts of science, mathematics
+                                    and robotics
+                                    Developed service catalog based on identified market need and categorized different
+                                    product & services
+                                    Conducted various seminars and talk show for creating awareness among teachers and
+                                    parents on STEM education
+                         </p>
+						<ul class="list">
+						    <li>Identified key decision makers in potential target customers and created proposition
+                                Generated financial report for potential investor·</li>
+							<li>Empowered close to 30000 Students, 500+ Teachers and 60+ Schools across Chhattisgarh.</li>
+							<li>Successfully established a venture with annual turnover of ~ INR 4 lakh
+                                Achieved break even on the investment of INR 6.5 lakhs at the end of 18 months.</li>
+							<li>Established partnership with various schools for developing practical learning lab and
+                                science center.</li>
+						</ul>
+					</div>-->
+					
+					
+
+					<!--     3rd experience  -->
+					<div class="experience margin-b-50">
+						<h4><b>INTERNSHIP</b></h4>
+						<h5 class="font-yellow"><b>JAN 2022 - April 2022</b></h5>
+						<h6 class="font-lite-black margin-t-10">BHARTI INFO TECH & SERVICES</h6>
+						<p class="margin-tb-20">
+						Working in Facerecognition and QRCode using different algorithm and opencv in raspberry pi for IOT projects.
+                        </p>
+						<p class="margin-tb-20">
+						www.bitsrobotech.com
+                        </p>
+						
+					</div>
+					
+				</div> <!-- col-sm-9 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- experience-section -->
+	
+	<section class="portfolio-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-3">
+					<div class="heading">
+						<h3><b>PROJECTS</b></h3>
+						<h6 class="font-lite-black"><b>MY WORK</b></h6>
+					</div>
+				</div><!-- col-sm-3 -->
+				
+				<div class="col-sm-12 col-md-9">
+					<div class="portfolioFilter clearfix margin-b-80">
+						<a href="#" data-filter="*" class="current"><b>ALL</b></a>
+						<a href="#" data-filter=".web-design"><b>Flutter – Chat App</b></a>
+						<a href="#" data-filter=".branding"><b>Object_Recognition App</b></a>
+						<a href="#" data-filter=".graphic-design"><b>Email_Assistant- Raspberry pi</b></a>
+						
+					</div><!-- portfolioFilter -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+			
+			<div class="portfolioContainer  margin-b-50">
+				
+				<div class="p-item web-design">
+					<a href="images/chat.jpeg" data-fluidbox>
+						<img src="images/chat_ui.jpeg" alt=""></a>
+				</div><!-- p-item -->
+				
+				<div class="p-item branding graphic-design">
+					<a href="images/object.jpeg" data-fluidbox>
+						<img src="images/object1.jpeg" alt=""></a>
+				</div><!-- p-item -->
+				
+			
+				
+				<div class="p-item graphic-design">
+					<a class="img" href="images/sagar/Image3.jpeg" data-fluidbox>
+						<img src="images/sagar/Image3.jpeg" alt=""></a>
+				</div><!-- p-item -->
+				
+				<div class="p-item branding graphic-design">
+					<a href="images/ai.jpeg" data-fluidbox>
+						<img src="images/ai.jpeg" alt=""></a>
+				</div><!-- p-item -->
+				
+				
+					
+				
+			
+			</div><!-- portfolioContainer -->
+		</div><!-- container -->
+	</section><!-- portfolio-section -->
+	
+	
+	<section class="education-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-3">
+					<div class="heading">
+						<h3><b>PROJECTS</b></h3>
+						
+					</div>
+				</div><!-- col-sm-3 -->
+				
+				<div class="col-sm-12 col-md-9">
+				
+
+
+					<div class="education-wrapper">
+						<div class="education margin-b-30">
+							<h4><b>Newflutter – Chat App</b></h4>
+							<h5 class="font-yellow"><b>Google Play Store Link:</b></h5>
+							<h6 class="font-lite-black margin-t-10">https://play.google.com/store/apps/details?id=com.anishyadav.newflutter</h6>
+							<p class="margin-tb-25">Newflutter is a chat app that is built with a simple yet intuitive UI. The
+                                                    app lets share photos, comment on other posts and even lets you chat
+                                                    with your friends and family. </p>
+						</div><!-- education -->
+						
+						
+						
+						<div class="education margin-b-30">
+							<h4><b>Object_Recognition App - Machine Learning Model</b></h4>
+							<h5 class="font-yellow"><b>Google Play Store Link :</b></h5>
+							<h6 class="font-lite-black margin-t-10">
+                            https://play.google.com/store/apps/detailsid=com.anishyadav.object_recognition</h6>
+							<p class="margin-tb-25"> Object recognition is a general term to describe a collection of related
+                              . computer vision tasks that involve identifying objects in digital . . </p>
+						</div><!-- education -->
+						
+						
+						
+						
+ 
+
+						
+						<div class="education margin-b-50">
+							<h4><b>Email_Assistant- Raspberry pi</b></h4>
+							<h5 class="font-yellow"><b>Link - https://cymric-hitches.000webhostapp.com/ </b></h5>
+							
+							<p class="margin-tb-20">Smart home control device is an open-source email empower where every one of the machines (light, fan, air conditioning and so on.) are associated with the raspberry pi board, and this board is associated with the email . </p>
+						</div><!-- education -->
+					</div><!-- education-wrapper -->
+				</div><!-- col-sm-9 -->
+			</div><!-- row -->
+		</div><!-- container -->
+		
+	</section><!-- education-section -->
+	
+	<footer>
+		<p class="copyright">
+			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ion-heart" aria-hidden="true"></i> by <a href="https://www.bitsbilaspur.com" target="_blank">www.bitsrobotech.com</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+		</p>
+	</footer>
+	
+	
+	<!-- SCIPTS -->
+	
+	<script src="common-js/jquery-3.2.1.min.js"></script>
+	
+	<script src="common-js/tether.min.js"></script>
+	
+	<script src="common-js/bootstrap.js"></script>
+	
+	<script src="common-js/isotope.pkgd.min.js"></script>
+	
+	<script src="common-js/jquery.waypoints.min.js"></script>
+	
+	<script src="common-js/progressbar.min.js"></script>
+	
+	<script src="common-js/jquery.fluidbox.min.js"></script>
+	
+	<script src="common-js/scripts.js"></script>
+	
+</body>
+</html>
